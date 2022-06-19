49878f7c55792c32ff88abc2af24047a991e33b6
Andrew Leech <andrew.leech@planetinnovation.com.au>
2022-06-19 10:36:36 +1000

    utokenize: Add module from pycopy v2.0.
    source: https://github.com/pfalcon/pycopy-lib/tree/6a5a47b82b84deed49f6f83c22e44620286f8100/utokenize

---
+++ python-stdlib/utokenize/example_test_dump.py
@@ -0,0 +1,11 @@
+import sys
+import utokenize as tokenize
+
+
+f = open(sys.argv[1], "r")
+for t in tokenize.generate_tokens(f.readline):
+    # print(t)
+    print(
+        "TokenInfo(type=%d (%s), string=%r, startl=%d)"
+        % (t.type, tokenize.tok_name[t.type], t.string, t.start)
+    )
